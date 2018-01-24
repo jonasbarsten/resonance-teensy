8c51290fd8a2664fd0b772a38df688b71804dd78
Jonas Barsten <jonasbarsten@gmail.com>
2018-01-24 15:37:39 +0100

    Creating test loop for solenoid and led

--- resonance-teensy.ino
+++ resonance-teensy.ino
@@ -1,14 +1,18 @@
 // Simple LED blink
 
+const int solenoid = 12;
 const int led = LED_BUILTIN;
 
 void setup() {
+  pinMode(solenoid, OUTPUT);
   pinMode(led, OUTPUT);
 }
 
 void loop() {
+  digitalWrite(solenoid, HIGH);
   digitalWrite(led, HIGH);
-  delay(1000);
+  delay(100);
+  digitalWrite(solenoid, LOW);
   digitalWrite(led, LOW);
   delay(1000);
 }
